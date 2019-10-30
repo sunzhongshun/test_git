ce1e8cacf5e0b96ecc8a99cd663509362853181a
sunzhongshun <zhongshun_sun@126.com>
2019-10-30 13:31:44 +0800

    自己的分支第一次提交

---
+++ src/com/shunshun/Hello.java
@@ -0,0 +1,13 @@
+package com.shunshun;
+
+/**
+ * @author 孙忠顺
+ * @create 2019-10-30 13:29
+ */
+public class Hello {
+
+	public static void main(String[] args){
+		System.out.println("hello world 2");
+	}
+
+}
