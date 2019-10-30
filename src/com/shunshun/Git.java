a3dbf0b1e30ae24d4d575de8705e52113e567095
sunzhongshun <zhongshun_sun@126.com>
2019-10-30 13:26:46 +0800

    add git.java

---
+++ src/com/shunshun/Git.java
@@ -0,0 +1,13 @@
+package com.shunshun;
+
+/**
+ * @author 孙忠顺
+ * @create 2019-10-30 13:25
+ */
+public class Git {
+
+	public static void main(String[] args){
+		System.out.println("hello world");
+	}
+
+}
